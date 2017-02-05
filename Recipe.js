2193c27faec9456c053d9a3ad9e157e464abf0b9
Peter Brumblay <peter@tyrconsulting.com>
2017-02-05 10:04:04 -0700

    adding Id to entity

--- Recipe.js
+++ Recipe.js
@@ -7,6 +7,7 @@ class Recipe {
     convertToExplicit()
     {
         return [
+            {  name: 'Id', value: this.recipe.Id, },
             {  name: 'Name', value: this.recipe.Name, },
             {  name: 'CategoryName', value: this.recipe.CategoryName, },
             {  name: 'Description', value: this.recipe.Description, },
